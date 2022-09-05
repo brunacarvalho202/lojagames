3250ac4292e24c3100f3ae270f1abc915ddfce5a
Bruna Bezerra <brubslubs2019@gmail.com>
2022-09-05 11:46:22 -0300

    camada de security adc

---
+++ src/main/java/com/lojadegames/lojadegames/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.lojadegames.lojadegames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.lojadegames.lojadegames.model.CategoriaModel;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+	public List<CategoriaModel> findAllByCategoriaContainingIgnoreCase(@Param("categoriamodel") String categoriamodel);
+}
